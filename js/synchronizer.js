a0be965a2b74d84c439725bec7fba14b13bbff9d
Dorian.Roques <dorian-roques@orange.fr>
2020-08-13 22:08:55 +0200

    Persmission fixing + Cookies managing fixing Added HTTPS version of the SquadGuessr domain to includes secure cookies Implemented presence cookie existence before to add it to prevent the extension from raising an error on setting this cookie Implemented filtering on lobby address during round posting to avoid posting on "io" or other non-lobby address cookies

--- js/synchronizer.js
+++ js/synchronizer.js
@@ -1,3 +1,5 @@
+const PRESENCE_COOKIE_NAME = "is_geo-sync_installed";
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	switch(request.type) {
 	case 'POST_PRESENCE':
@@ -10,8 +12,23 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function postPresenceCookie() {
-	// WARNING : Do not change it to httpS !!!
-	chrome.cookies.set({ url: "http://squad-guessr.herokuapp.com/", name: "is_geo-sync_installed", value: "true" });
+	var cookieDetails = {
+		url: "https://squad-guessr.herokuapp.com/",
+		secure: true
+	};
+
+	chrome.cookies.getAll(cookieDetails, (squadGuessrCookies) => {
+		var doesCookieExists = findGeoSyncInstalledCookie(squadGuessrCookies);
+
+		if(!doesCookieExists) {
+			// WARNING : Do not change it to httpS !!!
+			chrome.cookies.set({ url: "http://squad-guessr.herokuapp.com/", name: "is_geo-sync_installed", value: "false" }, (cookie) => {
+				console.log(JSON.stringify(cookie));
+				console.log(chrome.extension.lastError);
+				console.log(chrome.runtime.lastError);
+			});
+		}
+	});
 }
 
 function postRoundResult(request) {
@@ -32,20 +49,42 @@ function postRoundResult(request) {
 			if(lobbyAddress && requestPayload.challenge_id
 			&& requestPayload.round_order && requestPayload.round_score
 			&& requestPayload.total_score) {
-				var roundParams = '';
-				roundParams += 'lobby_address=' + lobbyAddress;
-				roundParams += '&challenge_address=' + requestPayload.challenge_id;
-				roundParams += '&round_order=' + requestPayload.round_order;
-				roundParams += '&round_score=' + requestPayload.round_score;
-				roundParams += '&total_challenge_score=' + requestPayload.total_score;
-				
-				var roundRequest = new XMLHttpRequest();
-				roundRequest.open('POST', targetUrl + "round", true);
+				if(isLobbyAddressValid(lobbyAddress)) {
+					var roundParams = '';
+					roundParams += 'lobby_address=' + lobbyAddress;
+					roundParams += '&challenge_address=' + requestPayload.challenge_id;
+					roundParams += '&round_order=' + requestPayload.round_order;
+					roundParams += '&round_score=' + requestPayload.round_score;
+					roundParams += '&total_challenge_score=' + requestPayload.total_score;
+					
+					var roundRequest = new XMLHttpRequest();
+					roundRequest.open('POST', targetUrl + "round", true);
 
-				roundRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				roundRequest.setRequestHeader("Authorization", jwt);
-				roundRequest.send(roundParams);
+					roundRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+					roundRequest.setRequestHeader("Authorization", jwt);
+					roundRequest.send(roundParams);
+				}
 			}
 		});
 	});
+}
+
+function findGeoSyncInstalledCookie(squadGuessrCookies) {
+	var doesCookieExists = false;
+
+	for(var i = 0; i < squadGuessrCookies.length && !doesCookieExists; i++) {
+		var squadGuessrCookie = squadGuessrCookies[i];
+
+		if(squadGuessrCookie.name == PRESENCE_COOKIE_NAME) {
+			doesCookieExists = true;
+		}
+	}
+
+	return doesCookieExists;
+}
+
+function isLobbyAddressValid(lobbyAddress) {
+	return lobbyAddress.length >= 10
+		&& lobbyAddress != PRESENCE_COOKIE_NAME
+		&& !lobbyAddress.includes("_");
 }
